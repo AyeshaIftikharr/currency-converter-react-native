ec3f839bf81076708c954aa99fd594eb298c0248
Ayesha iftikhar <ayesha.ifr@gmail.com>
2020-01-20 16:17:01 +0500

    Linted eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    quotes: ["error", "single", { "avoidEscape": true, "allowTemplateLiterals": true }],
-    'jsx-quotes': ["error", "prefer-single"],
-  }
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'jsx-quotes': ['error', 'prefer-single'],
+  },
 };
