f04c5a5fc9d93430324ee4146c13420817193720
Ayesha iftikhar <ayesha.ifr@gmail.com>
2020-01-22 21:02:50 +0500

    Added debug script to run react native debugger

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,26 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
 import { persistedReducer } from '../reducers';
 import { rootSaga } from '../sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-const middleware = [sagaMiddleware];
-if (process.env.NODE_ENV === 'development') {
-  middleware.push(logger);
-}
+// const middleware = [sagaMiddleware];
+// if (process.env.NODE_ENV === 'development') {
+//   middleware.push(logger);
+// }
 
-const store = createStore(persistedReducer, applyMiddleware(...middleware));
+const enhancer = compose(
+  applyMiddleware(sagaMiddleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f, // it can be
+);
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// export default createStore(reducers, composeEnhancers(applyMiddleware(...middleware)));
+
+const store = createStore(persistedReducer, enhancer);
 
 sagaMiddleware.run(rootSaga);
 
