e89be1d4cd554e0af7496ed7202d1107f2e8a570
Ayesha Iftikhar <ayesha.ifr@gmail.com>
2020-01-25 00:50:06 +0500

    Reverted Default theme change

--- storybook/index.js
+++ storybook/index.js
@@ -15,7 +15,7 @@ configure(() => {
 const StorybookUIRoot = getStorybookUI({ asyncStorage: null, onDeviceUI: false });
 
 export const App = () => (
-  <ThemeProvider theme={THEME_LOOKUP.ORANGE}>
+  <ThemeProvider theme={THEME_LOOKUP.BLUE}>
     <StorybookUIRoot />
   </ThemeProvider>
 );
